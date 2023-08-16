aa76f38b45cbae0c43003cd84efa4a1fabcf9e68
wobsoriano <sorianorobertc@gmail.com>
2023-08-16 01:04:29 -0700

    add organization page

---
+++ pages/organization.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { OrganizationProfile } from 'vue-clerk';
+</script>
+
+<template>
+  <div class="grid h-full place-items-center">
+    <OrganizationProfile />
+  </div>
+</template>
