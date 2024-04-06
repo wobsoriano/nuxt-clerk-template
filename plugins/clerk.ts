b0ae130dbb2d5a5ffb1225f8892edc2e19ded77f
wobsoriano <sorianorobertc@gmail.com>
2024-04-05 20:55:27 -0700

    update vue clerk options

--- plugins/clerk.ts
+++ plugins/clerk.ts
@@ -5,22 +5,20 @@ export default defineNuxtPlugin(async (nuxtApp) => {
 
   nuxtApp.vueApp.use(clerkPlugin, {
     publishableKey,
-    options: {
-      appearance: {
-        // variables: { colorPrimary: "#000000" },
-        // elements: {
-        //   formButtonPrimary:
-        //     "bg-black border border-black border-solid hover:bg-white hover:text-black",
-        //   socialButtonsBlockButton:
-        //     "bg-white border-gray-200 hover:bg-transparent hover:border-black text-gray-600 hover:text-black",
-        //   socialButtonsBlockButtonText: "font-semibold",
-        //   formButtonReset:
-        //     "bg-white border border-solid border-gray-200 hover:bg-transparent hover:border-black text-gray-500 hover:text-black",
-        //   membersPageInviteButton:
-        //     "bg-black border border-black border-solid hover:bg-white hover:text-black",
-        //   card: "bg-[#fafafa]",
-        // },
-      },
+    appearance: {
+      // variables: { colorPrimary: "#000000" },
+      // elements: {
+      //   formButtonPrimary:
+      //     "bg-black border border-black border-solid hover:bg-white hover:text-black",
+      //   socialButtonsBlockButton:
+      //     "bg-white border-gray-200 hover:bg-transparent hover:border-black text-gray-600 hover:text-black",
+      //   socialButtonsBlockButtonText: "font-semibold",
+      //   formButtonReset:
+      //     "bg-white border border-solid border-gray-200 hover:bg-transparent hover:border-black text-gray-500 hover:text-black",
+      //   membersPageInviteButton:
+      //     "bg-black border border-black border-solid hover:bg-white hover:text-black",
+      //   card: "bg-[#fafafa]",
+      // },
     },
   })
 })
