a7e9076a8f554b1cda96920bc814f18405e3be10
wobsoriano <sorianorobertc@gmail.com>
2024-05-10 18:19:38 -0700

    cleanup

--- plugins/clerk.client.ts
+++ plugins/clerk.client.ts
@@ -7,9 +7,10 @@ export default defineNuxtPlugin(async (nuxtApp) => {
   const clerk = new Clerk(publishableKey)
 
   // Instead of using the `vue-clerk` plugin, we can use this internal function to create a Clerk instance.
-  provideClerkToApp(nuxtApp.vueApp, clerk, {}, {
+  provideClerkToApp(nuxtApp.vueApp, clerk, {
     isClerkLoaded,
     shouldLoadClerk: false,
+    clerkOptions: {}, // Optional since we run `clerk.load` manually
   })
 
   // This will make sure that the clerk library is loaded in the client first before moving on to the next middleware.
