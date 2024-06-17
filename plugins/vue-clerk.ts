f0a7ddd8b3e2c35661410607648f3257f0848b0d
wobsoriano <sorianorobertc@gmail.com>
2024-06-17 00:44:51 -0700

    add some docs

--- plugins/vue-clerk.ts
+++ plugins/vue-clerk.ts
@@ -6,6 +6,8 @@ export default defineNuxtPlugin((nuxtApp) => {
   const publishableKey = useRuntimeConfig().public.clerkPublishableKey as string
   const serverInitialState = useState<SignedInAuthObject | SignedOutAuthObject | undefined>('clerk-initial-state', () => undefined)
 
+  // Installing the `withClerkMiddleware` from `h3-clerk` adds an `auth` object to the context.
+  // We can then use the `auth` object to get the initial state of the user.
   if (import.meta.server) {
     const authContext = nuxtApp.ssrContext?.event.context.auth
     serverInitialState.value = authContext ? JSON.parse(JSON.stringify(authContext)) : undefined
