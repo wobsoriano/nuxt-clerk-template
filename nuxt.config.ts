384e5eb5eeba3a5762b360c28c6dd2caa5a652fc
wobsoriano <sorianorobertc@gmail.com>
2024-07-02 10:34:10 -0700

    bump nuxt version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,15 +2,19 @@
 export default defineNuxtConfig({
   devtools: { enabled: false },
   modules: ['@nuxtjs/tailwindcss', '@nuxt/image', '@nuxtjs/google-fonts'],
+
   runtimeConfig: {
     public: {
       publishableKey: process.env.CLERK_PUBLISHABLE_KEY,
     }
   },
+
   googleFonts: {
     families: {
       Inter: true,
     },
     subsets: 'latin',
   },
+
+  compatibilityDate: '2024-07-02',
 })
