9270e20ea99a9db105a78ce944c6c6679a2fc504
wobsoriano <sorianorobertc@gmail.com>
2024-08-31 07:33:54 -0700

    chore: initial fix for component types

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,32 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/image'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/image', 'vue-clerk/nuxt'],
+  build: {
+    transpile: ['vue-clerk']
+  },
 
-  runtimeConfig: {
-    public: {
-      publishableKey: process.env.CLERK_PUBLISHABLE_KEY,
+  clerk: {
+    publishableKey: process.env.CLERK_PUBLISHABLE_KEY,
+    signInForceRedirectUrl: '/dashboard',
+    signUpForceRedirectUrl: '/dashboard',
+    signInUrl: '/sign-in',
+    signUpUrl: '/sign-up',
+    afterSignOutUrl: '/',
+    appearance: {
+      variables: { colorPrimary: '#000000' },
+      elements: {
+        formButtonPrimary:
+                'bg-black border border-black border-solid hover:bg-white hover:text-black',
+        socialButtonsBlockButton:
+                'bg-white border-gray-200 hover:bg-transparent hover:border-black text-gray-600 hover:text-black',
+        socialButtonsBlockButtonText: 'font-semibold',
+        formButtonReset:
+                'bg-white border border-solid border-gray-200 hover:bg-transparent hover:border-black text-gray-500 hover:text-black',
+        membersPageInviteButton:
+                'bg-black border border-black border-solid hover:bg-white hover:text-black',
+        card: 'bg-[#fafafa]',
+      },
     },
   },
 
