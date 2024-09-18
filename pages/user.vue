86e1b703181899c0a35bc1e44960e9672ed76751
wobsoriano <sorianorobertc@gmail.com>
2024-09-18 13:27:19 -0700

    remove unused files

--- pages/user.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-definePageMeta({ middleware: 'auth', auth: { guestRedirectUrl: '/sign-in' } })
-</script>
-
-<template>
-  <div class="flex justify-center py-24">
-    <UserProfile />
-  </div>
-</template>
